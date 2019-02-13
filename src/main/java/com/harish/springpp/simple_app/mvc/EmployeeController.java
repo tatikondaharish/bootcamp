5f7b99b81ff96a63fa5e7c63cc891f1a5b5e4219
chandrasekharnallam <chandrasekharnallam@10.235.125.16>
2019-02-13 00:54:16 -0800

    Fixed DataType Issue

--- src/main/java/com/harish/springpp/simple_app/mvc/EmployeeController.java
+++ src/main/java/com/harish/springpp/simple_app/mvc/EmployeeController.java
@@ -1,23 +1,32 @@
 package com.harish.springpp.simple_app.mvc;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.harish.springpp.simple_app.mvc.entity.EmployeeDetails;
 
-@Controller
+@RestController
 @RequestMapping(value="/employee")
 public class EmployeeController {
 	
-	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> employeeDetails (@RequestBody EmployeeDetails employeeDetails) {
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<EmployeeDetails> employeeDetails (@RequestBody(required = false) EmployeeDetails employeeDetails) {
 		return new ResponseEntity(employeeDetails,HttpStatus.OK);
 	}
 	
+	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody EmployeeDetails getEmployeeDetails () {
+		EmployeeDetails employeeDetails = new EmployeeDetails();
+		employeeDetails.setFirstName("Chandra");
+		employeeDetails.setLastName("Nallam");
+		employeeDetails.setEmailId("csnallam1@gmail.com");
+		employeeDetails.setDepartment("department");
+		return employeeDetails;
+	}
 	
 }
--- src/main/java/com/harish/springpp/simple_app/mvc/entity/EmployeeDetails.java
+++ src/main/java/com/harish/springpp/simple_app/mvc/entity/EmployeeDetails.java
@@ -2,36 +2,46 @@ package com.harish.springpp.simple_app.mvc.entity;
 
 public class EmployeeDetails {
 
-	private String firstname;
-	private String lastname;
-	private String emailid;
-	private String department;
+	public String firstName;
+	public String lastName;
+	public String emailId;
+	public String department;
+	
 	public EmployeeDetails() {
 		
 	}
-	public String getFirstname() {
-		return firstname;
+
+	public String getFirstName() {
+		return firstName;
 	}
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
-	public String getLastname() {
-		return lastname;
+
+	public String getLastName() {
+		return lastName;
 	}
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
-	public String getEmailid() {
-		return emailid;
+
+	public String getEmailId() {
+		return emailId;
 	}
-	public void setEmailid(String emailid) {
-		this.emailid = emailid;
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
 	}
+
 	public String getDepartment() {
 		return department;
 	}
+
 	public void setDepartment(String department) {
 		this.department = department;
 	}
+	
 	
 }
